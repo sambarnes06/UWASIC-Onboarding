ae6939ea252d2f53a835f26eca0222c97e156d76
Sam Barnes <sambarnesdev@icloud.com>
2025-05-24 15:03:16 -0400

    Fixed overdriven pins

--- src/project.v
+++ src/project.v
@@ -17,7 +17,6 @@ module tt_um_uwasic_onboarding_sam_barnes (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 8'hFF;
 
