0490ad0e041734dd985687222ae0b3d04264066a
Sam Barnes <sambarnesdev@icloud.com>
2025-05-24 14:39:14 -0400

    Duplicate variable typo fix

--- src/project.v
+++ src/project.v
@@ -25,7 +25,6 @@ module tt_um_uwasic_onboarding_sam_barnes (
   wire [7:0] en_reg_out_15_8;
   wire [7:0] en_reg_pwm_7_0;
   wire [7:0] en_reg_pwm_15_8;
-  wire [7:0] en_reg_out_15_8;
   wire [7:0] pwm_duty_cycle;
 
   pwm_peripheral pwm_peripheral_inst (
