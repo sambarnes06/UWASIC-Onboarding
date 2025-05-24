d4341080b406083e1bf2831db5f6158da0fbcb47
Sam Barnes <sambarnesdev@icloud.com>
2025-05-24 19:40:19 -0400

    Reverted fix

--- test/test.py
+++ test/test.py
@@ -106,7 +106,7 @@ async def test_spi(dut):
     dut._log.info("Test project behavior")
     dut._log.info("Write transaction, address 0x00, data 0xF0")
     ui_in_val = await send_spi_transaction(dut, 1, 0x00, 0xF0)  # Write transaction
-    assert dut.uo_out.value == 0xF0, f'Expected 0xF0, got {dut.uo_out.value}'
+    assert dut.uo_out.value == 0xF0, f"Expected 0xF0, got {dut.uo_out.value}"
     await ClockCycles(dut.clk, 1000) 
 
     dut._log.info("Write transaction, address 0x01, data 0xCC")
